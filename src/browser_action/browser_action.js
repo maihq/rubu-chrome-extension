891175576be08465af283e86b554e52ff56c504a
David Frank <bitinn@gmail.com>
2015-08-19 18:53:45 +0800

    working on save api

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,42 +1,82 @@
 
 var doc = document;
-var base_url = 'https://rubu.me';
+var base_url = 'https://mai.dev/api/v1';
+var popup_id = 'share-popup-message';
+
+function clearPopup (id) {
+	var container = doc.getElementById(id);
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
+
+function setCustomMessage (id, elem) {
+	// clean up popup first
+	clearPopup(id);
+
+	// update popup
+	doc.getElementById(id).appendChild(elem);
+};
 
 function setMessage (id, msg) {
-	msg = msg || '';
-	doc.getElementById(id).innerHTML = msg;
+	// clean up popup first
+	clearPopup(id);
+
+	// update popup
+	msg = msg || 'unknown message';
+	msg = doc.createTextNode(msg);
+	doc.getElementById(id).appendChild(msg);
 };
 
 function getMessage (name, data) {
-	data = data || '';
+	data = data || [];
 	return chrome.i18n.getMessage(name, data);
 };
 
-function getMessageName (id) {
-	return doc.getElementById(id).textContent;
-};
-
 function sharePage (tabs) {
-	if (tabs.length !== 1) {
+	// must have an active tab
+	if (tabs.length === 0) {
 		return;
 	}
 
 	var tab = tabs[0];
 
+	// active tab must contain url
 	if (!tab || !tab.url || tab.url.indexOf('http') !== 0) {
 		return;
 	}
 
-	window.open(base_url + '/?share=' + encodeURIComponent(tab.url));
+	// send to server
+	var fetchUrl = base_url + '/stash';
+	var fetchOpts = {
+		method: 'POST'
+		, headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+		, body: 'url=' + encodeURIComponent(tab.url) + '&title=' + tab.title
+	};
+
+	fetch(fetchUrl, fetchOpts).then(function (res) {
+		// create popup message
+		var title = tab.title || 'unknown title';
+		var text;
+
+		if (!res.ok) {
+			text = getMessage('save_failed_message', [title]);
+			setMessage(popup_id, text);
+			return;
+		}
+
+		text = getMessage('save_success_message', [title]);
+		setMessage(popup_id, text);
+	});
 };
 
-function init() {
-	var id = 'share-popup-message';
-	var name = getMessageName(id);
-	var message = getMessage(name);
-	setMessage(id, message);
+function share() {
+	var text = getMessage('save_progress_message');
+	setMessage(popup_id, text);
 
 	chrome.tabs.query({ active: true, lastFocusedWindow: true }, sharePage);
 };
 
-doc.addEventListener('DOMContentLoaded', init);
+doc.addEventListener('DOMContentLoaded', share);
